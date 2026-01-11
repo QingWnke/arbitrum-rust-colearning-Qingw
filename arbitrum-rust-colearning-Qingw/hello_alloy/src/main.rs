b2745dd64fd361aa1dcaa86e7c83c077b9332f91
Qingw <a56142541@outlook.com>
2026-01-11 23:23:47 +0800

    Update main.rs

--- arbitrum-rust-colearning-Qingw/hello_alloy/src/main.rs
+++ arbitrum-rust-colearning-Qingw/hello_alloy/src/main.rs
@@ -1,4 +1,3 @@
- 
 use alloy::providers::{Provider, ProviderBuilder};
 use alloy::primitives::Address;
 use std::error::Error;
@@ -13,23 +12,22 @@ sol! {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     let rpc_url = "https://arbitrum-sepolia-rpc.publicnode.com".parse()?;
- 
     let provider = ProviderBuilder::new().connect_http(rpc_url); 
     
     let latest_block = provider.get_block_number().await?;
-
     println!("Latest block number: {latest_block}");
     println!("Hello web3");
-   
 
     let contract_address: Address = "0x74Ceff170fB92D22d04622Eb94Fa084026F3fb76".parse()?;
-    
     let contract = HelloWeb3::new(contract_address, provider);
     
-    let result = contract.hello_web3().call().await?;
-    println!("合约返回: {}", result);
+    let result = contract.hello_web3().call().await;
+
+    match result {
+        Ok(value) => println!("合约返回: {}", value),
+        Err(e) => eprintln!("调用合约失败: {}", e),
+    }
  
     Ok(())
 }
